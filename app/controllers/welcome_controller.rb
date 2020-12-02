d43a050eb6d7f719f9e2404142003635a8832107
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-12-01 23:20:43 -0600

    added generic Devise+GoogleOAuth2 User signuplogin

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,11 @@
+class WelcomeController < ApplicationController
+protect_from_forgery
+before_action :authenticate_user!
+
+def home
+    if user_signed_in?
+        @user = current_user
+    end
+end
+
+end
