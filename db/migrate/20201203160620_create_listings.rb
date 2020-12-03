8899634e6c9ae02e5e7d854591b1f19bd2c01d36
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-12-03 10:07:44 -0600

    generated Listing resource

---
+++ db/migrate/20201203160620_create_listings.rb
@@ -0,0 +1,21 @@
+class CreateListings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :listings do |t|
+      t.string :title
+      t.string :description
+      t.integer :user_id
+      t.integer :seller_user_id
+      t.integer :auction_id
+      t.decimal :reserve_price
+      t.datetime :start_time
+      t.datetime :end_time
+      t.intger :starting_bid_id
+      t.integer :winning_bid_id
+      t.string :item_condition
+      t.integer :item_qty
+      t.integer :lot_size
+
+      t.timestamps
+    end
+  end
+end
