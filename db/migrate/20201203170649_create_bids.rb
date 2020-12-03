3abf40875e354da43a71262b6b8b9331638aff83
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-12-03 11:07:05 -0600

    created Bid model

---
+++ db/migrate/20201203170649_create_bids.rb
@@ -0,0 +1,14 @@
+class CreateBids < ActiveRecord::Migration[6.0]
+  def change
+    create_table :bids do |t|
+      t.integer :listing_id
+      t.integer :buyer_id
+      t.integer :user_id
+      t.decimal :amount
+      t.decimal :max_bid
+      t.boolean :winning_bid
+
+      t.timestamps
+    end
+  end
+end
