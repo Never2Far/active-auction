f28d5f070bdb35e5315ed374eb9bd3edcb423229
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-12-03 11:10:25 -0600

    generated Organization resource

---
+++ db/migrate/20201203171006_create_organizations.rb
@@ -0,0 +1,13 @@
+class CreateOrganizations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :organizations do |t|
+      t.string :name
+      t.string :department
+      t.string :type
+      t.integer :admin_user_id
+      t.string :contact_info
+
+      t.timestamps
+    end
+  end
+end
