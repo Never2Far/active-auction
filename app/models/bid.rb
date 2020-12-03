f413a4e06f6c1ace8b07b287bbc554d2e34e8cb0
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-12-03 15:49:31 -0600

    added display method to get string repres. bid

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -2,4 +2,10 @@ class Bid < ApplicationRecord
     belongs_to :listing
     belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id'
     # has_one :auction, through: :listing
+
+
+
+    def display
+        "#{self.listing.title}, Bid: $#{self.amount} (Max Bid: $#{self.max_bid})"
+    end
 end
