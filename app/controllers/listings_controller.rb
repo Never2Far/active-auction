64623c8145e71ea7264a5a0cbb787b9dc8181ff1
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-12-04 00:20:23 -0600

    added listings controller and show view

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,33 @@
 class ListingsController < ApplicationController
+
+
+    def new
+        @listing = Listing.new
+    end
+
+    def create
+        @listing = Listing.new(listing_params)
+
+        if @listing.save
+            redirect_to listing_path(@listing)
+        end
+
+        #error
+
+        redirect_to new_listing_path
+
+    end
+
+    def show
+        @listing = Listing.find_by(id: params[:id])
+        render 'listings/show'
+    end
+
+private
+
+def listing_params
+    params.require(:listing).permit(:id, :item_id, :item_name, :title, :description, :reserve_price, :start_time, :end_time, :item_condition, :item_qty, :lot_size, :active)
+end
+
+
 end
