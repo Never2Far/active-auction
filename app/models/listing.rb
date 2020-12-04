3001f11da44a550e8f3da60e3a0d245802000c32
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-12-04 00:23:12 -0600

    added methods to display bids

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,26 @@ class Listing < ApplicationRecord
     belongs_to :seller, class_name: 'User', foreign_key: 'seller_user_id'
     has_and_belongs_to_many :items
     has_many :bids
+    belongs_to :auction
+
+
+    def current_bid
+        self.bids.any? ? self.bids.last : nil
+    end
+
+    def current_bid_amount
+        if self.current_bid
+            self.current_bid.amount
+        else
+            "0.00"
+        end
+    end
+
+    def item
+        self.items.first
+    end
+
+    # def auction
+    #     Auction.find_by(id: self.auction_id)
+    # end
 end
