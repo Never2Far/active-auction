65540100b310a5ee8c2333c08d8be7442a549332
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-12-04 00:21:31 -0600

    added auctions controller and views

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,2 +1,35 @@
 class AuctionsController < ApplicationController
+    # protect_from_forgery
+    # before_action :authenticate_user!
+
+
+
+    def index
+        @auctions = Auction.all
+    end
+
+    def new
+        @auction = Auction.new
+    end
+
+    def create
+        @auction = Auction.new(auction_params)
+
+        if @auction.save
+            redirect_to auction_path(@auction)
+        end
+
+        redirect_to new_auction_page
+    end
+
+    def show
+        @auction = Auction.find_by(id: params[:id])
+    end
+
+    private
+
+    def auction_params
+        params.require(:auction).permit(:name, :organization, :private, :type, :start_time, :end_time, :admin_id)
+    end
+
 end
