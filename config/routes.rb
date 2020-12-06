b0835f6b65094a1ca5436303b3d4c450aa6a4c22
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-12-06 10:29:26 -0600

    added sign_up and dashboard routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   root to: 'welcome#home'
 
   devise_scope :user do
+    get 'users/sign_up' => 'devise/registrations#new'
+    get 'sign_up' => 'devise/registrations#new'
     get 'sign_out' => 'devise/sessions#destroy'
     get 'users/sign_out' => 'devise/sessions#destroy'
     get 'sign_in' => 'devise/sessions#new'
     delete 'sign_out', :to => 'devise/sessions#destroy'#, :as => :destroy_user_session
     get 'users/:id' => 'users#show'
+    get 'dashboard' => 'users#dashboard'
   end
   
   devise_for :users, controllers: {sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks'}
