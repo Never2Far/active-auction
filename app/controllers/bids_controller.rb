d7e93295446b2e5bd26de17a289e6310e4ac5140
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-12-06 19:38:18 -0600

    built out bid resource actions

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,8 +1,38 @@
 class BidsController < ApplicationController
 
-def show
-@bid = Bid.find_by(id: params[:id])
+
+def index
+    @bids = Bid.all
 end
 
+def show
+    @bid = Bid.find_by(id: params[:id])
+end
+
+def create
+    @bid = Bid.new(bid_params)
+    @listing = Listing.find(params.require(:listing_id))
+    if @bid.amount <= @listing.current_bid.amount
+        flash[:alert] = "Invalid bid: Please enter a bid greater than the listing\'s current bid"
+        redirect_to listing_path(@listing) and return
+    end
+    @bid.listing = @listing
+    @bid.buyer = current_user
+
+    unless !@bid.save
+        redirect_to listing_path(@listing)
+    else
+        ##error message (flash)
+        redirect_to listing_path(@listing)
+    end
+
+end
+
+
+private
+
+def bid_params
+    params.require(:bid).permit(:listing_id, :buyer_id, :amount, :max_bid)
+end
 
 end
