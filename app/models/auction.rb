e657caeb5934e74b6a9dac01f93d3105e638575e
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-12-08 10:21:33 -0600

    added method to find auction's admin

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -3,4 +3,9 @@ class Auction < ApplicationRecord
     belongs_to :admin, class_name: 'User', foreign_key: 'admin_id'
     has_many :items, through: :listings
     has_many :bids, through: :listings
+
+
+    def admin
+        User.find(self.admin_id)
+    end
 end
