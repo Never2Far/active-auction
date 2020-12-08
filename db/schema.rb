4d2a0e47f73ede76a30b02727db7041b2769cf47
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-12-08 14:32:17 -0600

    added username field to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_040043) do
+ActiveRecord::Schema.define(version: 2020_12_08_171546) do
 
   create_table "auctions", force: :cascade do |t|
     t.string "name"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 2020_12_08_040043) do
     t.string "provider"
     t.string "uid"
     t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
