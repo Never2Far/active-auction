579354d244afed14bf0ad123371092a59678e3f1
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-12-08 14:30:27 -0600

    removed buyer method (no longer needed)

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -3,11 +3,13 @@ class Bid < ApplicationRecord
     belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id'
     # has_one :auction, through: :listing
 
-    def buyer
-        User.find_by(id: self.buyer_id)
-    end
+    # def buyer
+    #     User.find_by(id: self.buyer_id)
+    # end
 
     def display
         "#{self.listing.title}, Bid: $#{self.amount} (Max Bid: $#{self.max_bid})"
     end
+
+
 end
