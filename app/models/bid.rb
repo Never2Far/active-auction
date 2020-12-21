177c8ebe8cffc07daa6bae22c8770a534501a280
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-12-21 13:29:49 -0600

    added bid validity checks and error messages

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -7,8 +7,28 @@ class Bid < ApplicationRecord
     #     User.find_by(id: self.buyer_id)
     # end
 
+    def errors?
+        errors = []
+        if self.buyer == self.listing.seller
+            errors << "You cannot bid on your own listing!"
+        # elsif (self.amount <= self.listing.current_bid_amount)
+        # errors << "Invalid bid: Please enter a bid greater than the listing\'s current bid"
+        else (self.amount < (self.listing.next_valid_bid))
+            errors << "Invalid bid: Please enter a bid of #{number_to_currency(self.listing.next_valid_bid)} or more"
+        end
+        if errors.any?
+            return errors
+        else
+            return false
+        end
+    end
+    
+    
+
+
+
     def display
-        "#{self.listing.title}, Bid: $#{self.amount} (Max Bid: $#{self.max_bid})"
+        "#{self.listing.title}, Bid: $#{self.amount}"
     end
 
 
