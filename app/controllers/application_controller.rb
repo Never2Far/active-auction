102f5b222d171a61458f1394fd627df4c84262b4
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-12-27 22:25:27 -0600

    change default Devise after_sign_in_path to dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
     def search
     render 'search'
     end
+
+    def after_sign_in_path_for(resource)
+        stored_location_for(resource) ||
+    if resource.is_a?(User) && user_signed_in?
+      '/dashboard'
+    else
+      super
+    end
+      end
 end
