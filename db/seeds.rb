5fa1cd078c1b6a4ca01a5afc126070f72d9dc6ae
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-01-04 08:43:20 -0600

    created a few test user accounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Add Users
+
+user = User.new
+    user.name = "test test"
+    user.username = "test"
+    user.email = "test@test.com"
+    user.password = "testtest"
+    user.password_confirmation = "testtest"
+    user.save!
+
+
+10.times do
+    passwrd = Faker::Internet.password
+    user = User.new
+        user.name = Faker::Name.unique.name, 
+        user.username = Faker::Internet.unique.username,
+        user.email = Faker::Internet.unique.email,
+        user.password = passwrd,
+        user.password_confirmation = passwrd
+    user.save!
+end
+
+# Faker::Name.unique.clear
