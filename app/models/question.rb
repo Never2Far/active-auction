d59eb3edecd667a666c04c1546dfa9965a76fb76
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-01-15 10:40:08 -0600

    added scope methods

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,13 @@
 class Question < ApplicationRecord
+scope :answered, -> {where(answered: true)}
+scope :unanswered, -> {where(answered: false)}
+
+
     belongs_to :listing
     belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id'
+
+    def status
+        self.answer ? "Answered" : "Not Answered"
+    end
+
 end
