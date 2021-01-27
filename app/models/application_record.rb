62acc220f3528848d19142ee9502777118a4f075
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-01-27 09:59:41 -0600

    extracted set_end_date method to be shared by both auctions and listings

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,4 +4,12 @@ class ApplicationRecord < ActiveRecord::Base
   def number_to_currency(number)
   ActiveSupport::NumberHelper.number_to_currency(number)
   end
+
+  def set_end_date(auctionOrListing) #accepts instances of Auction or Listing
+    t = auctionOrListing.start_time
+    d = auctionOrListing.start_date
+    auctionOrListing.start_date = DateTime.new(d.year, d.month, d.day, t.hour, t.min)
+    auctionOrListing.end_date = (auctionOrListing.start_date + auctionOrListing.duration.days).strftime("%b %-d, %Y %H:%M")
+  end
+  
 end
